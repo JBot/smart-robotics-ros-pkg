63ea23243090914fe4675ccb2806f4beea07395e
Jbot <kriegel.joffrey@gmail.com>
2011-08-21 22:54:03 +0200

    Now use rosserial.

--- maximus_position/src/maximus_transform_pose.cpp
+++ maximus_position/src/maximus_transform_pose.cpp
@@ -1,106 +1,10 @@
-/*
-#include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
-
-int main(int argc, char** argv){
-  ros::init(argc, argv, "odometry_publisher");
-
-  ros::NodeHandle n;
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
-  tf::TransformBroadcaster odom_broadcaster;
-
-  double x = 0.0;
-  double y = 0.0;
-  double th = 0.0;
-
-  double vx = 0.1;
-  double vy = -0.1;
-  double vth = 0.1;
-
-  ros::Time current_time, last_time;
-  current_time = ros::Time::now();
-  last_time = ros::Time::now();
-
-  ros::Rate r(10.0);
-  while(n.ok()){
-    current_time = ros::Time::now();
-
-    //compute odometry in a typical way given the velocities of the robot
-    double dt = (current_time - last_time).toSec();
-    double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-    double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-    double delta_th = vth * dt;
-
-    x += delta_x;
-    y += delta_y;
-    th += delta_th;
-
-    //since all odometry is 6DOF we'll need a quaternion created from yaw
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
-
-    //first, we'll publish the transform over tf
-    geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "base_link";
-
-    odom_trans.transform.translation.x = x;
-    odom_trans.transform.translation.y = y;
-    odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation = odom_quat;
-
-    //send the transform
-    odom_broadcaster.sendTransform(odom_trans);
-
-    //next, we'll publish the odometry message over ROS
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = "odom";
-
-    //set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
-
-    //set the velocity
-    odom.child_frame_id = "base_link";
-    odom.twist.twist.linear.x = vx;
-    odom.twist.twist.linear.y = vy;
-    odom.twist.twist.angular.z = vth;
-
-    //publish the message
-    odom_pub.publish(odom);
-
-    last_time = current_time;
-    r.sleep();
-  }
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #include "ros/ros.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseStamped.h"
 #include <maximus_position/AvrPose.h>
 #include <maximus_position/AvrVel.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 #include <nav_msgs/Odometry.h>
 
 #include <sys/time.h>
@@ -123,43 +27,41 @@ int main(int argc, char** argv){
 class TransformPose {
   public:
     TransformPose();
-    void publish_all(void);
+    void publish_all(tf::TransformListener& listener);
 
-     ros::Subscriber avrpose_sub_;
-     ros::Publisher pose_pub;
-     ros::Subscriber cmd_vel_sub_;
+//     ros::Subscriber tf_sub_;
+//     ros::Publisher pose_pub;
      ros::Publisher odom_pub;
-     ros::Publisher avrvel_pub;
 
 
   private:
-    void poseCallback(const maximus_position::AvrPose::ConstPtr & pose);
-    void velCallback(const geometry_msgs::Twist::ConstPtr & vel);
+//    void tfCallback(const tf::tfMessage::ConstPtr & mytf);
     void rotate(double heading, double attitude, double bank, geometry_msgs::PoseStamped * pose);
      ros::NodeHandle nh;
 
-     geometry_msgs::PoseStamped my_maximus_pose;
      nav_msgs::Odometry my_maximus_odom;
-     maximus_position::AvrVel my_maximus_vel;
+     double old_x;
+     double old_y;
+     double old_theta;
+     ros::Time old_time;
 
-     tf::TransformBroadcaster odom_broadcaster;
 
 };
 
 TransformPose::TransformPose()
 {
-
+/*
     // Joystick suscriber
     avrpose_sub_ = nh.subscribe < maximus_position::AvrPose > ("avrpose", 30, &TransformPose::poseCallback, this);
     // Set a Laser scan sensor for the robot
     pose_pub = nh.advertise < geometry_msgs::PoseStamped > ("avr_maximus_pose", 40);
 
     cmd_vel_sub_ = nh.subscribe < geometry_msgs::Twist > ("cmd_vel", 30, &TransformPose::velCallback, this);
-
+*/
     odom_pub = nh.advertise < nav_msgs::Odometry > ("odom", 50);
 
-    avrvel_pub = nh.advertise < maximus_position::AvrVel > ("avrvel", 30);
-
+//    avrvel_pub = nh.advertise < maximus_position::AvrVel > ("avrvel", 30);
+/*
     my_maximus_pose.header.frame_id = "/odom";
     my_maximus_pose.header.stamp = ros::Time::now();
 
@@ -170,7 +72,7 @@ TransformPose::TransformPose()
     my_maximus_pose.pose.orientation.y = 0;
     my_maximus_pose.pose.orientation.z = 0;
     my_maximus_pose.pose.orientation.w = 0;
-
+*/
     my_maximus_odom.header.frame_id = "/odom";
     my_maximus_odom.child_frame_id = "/base_link";
     my_maximus_odom.header.stamp = ros::Time::now();
@@ -187,10 +89,16 @@ TransformPose::TransformPose()
     my_maximus_odom.twist.twist.linear.y = 0;
     my_maximus_odom.twist.twist.angular.z = 0;
 
+    old_x = 0;
+    old_y = 0;
+    old_theta = 0;
+    old_time = my_maximus_odom.header.stamp;
+
+/*
     my_maximus_vel.vx = 0;
     my_maximus_vel.vy = 0;
     my_maximus_vel.vth = 0;
-
+*/
 
 }
 
@@ -213,7 +121,7 @@ void TransformPose::rotate(double heading, double attitude, double bank, geometr
     pose->pose.orientation.z = c1 * s2 * c3 - s1 * c2 * s3;
 }
 
-
+/*
 void TransformPose::poseCallback(const maximus_position::AvrPose::ConstPtr & pose)
 {
     float tmp = 0.0;
@@ -266,19 +174,70 @@ void TransformPose::velCallback(const geometry_msgs::Twist::ConstPtr & vel)
 {
 
 
-    my_maximus_vel.vx = vel->linear.x;
-    my_maximus_vel.vy = vel->linear.y;
-    my_maximus_vel.vth = vel->angular.z;
+    my_maximus_vel.vx = ((float) ((int)(vel->linear.x * 20) )) / (float)20;
+    my_maximus_vel.vy = ((float) ((int)(vel->linear.y * 20) )) / (float)20;
+    my_maximus_vel.vth = ((float) ((int)(vel->angular.z * 20) )) / (float)20;
 
     avrvel_pub.publish(my_maximus_vel);
 
 }
-
-void TransformPose::publish_all(void)
+*/
+void TransformPose::publish_all(tf::TransformListener& listener)
 {
 
-    // Publish the laser scan values
-    TransformPose::pose_pub.publish(my_maximus_pose);
+        geometry_msgs::PoseStamped odom_pose;
+        odom_pose.header.frame_id = "/base_link";
+
+        //we'll just use the most recent transform available for our simple example
+        odom_pose.header.stamp = ros::Time();
+
+        //just an arbitrary point in space
+        odom_pose.pose.position.x = 0.0;
+        odom_pose.pose.position.y = 0.0;
+        odom_pose.pose.position.z = 0.0;
+
+        odom_pose.pose.orientation.x = 0.0;
+        odom_pose.pose.orientation.y = 0.0;
+        odom_pose.pose.orientation.z = 0.0;
+        odom_pose.pose.orientation.w = 1.0;
+
+
+        try{
+                ros::Time now = ros::Time::now();
+                listener.waitForTransform("/odom", "/base_link", now, ros::Duration(5.0));
+                geometry_msgs::PoseStamped base_pose;
+                listener.transformPose("/odom", odom_pose, base_pose);
+
+		my_maximus_odom.header.stamp = base_pose.header.stamp;
+		my_maximus_odom.pose.pose.position.x = base_pose.pose.position.x;
+		my_maximus_odom.pose.pose.position.y = base_pose.pose.position.y;
+		my_maximus_odom.pose.pose.position.z = base_pose.pose.position.z;
+		my_maximus_odom.pose.pose.orientation = base_pose.pose.orientation;
+
+		my_maximus_odom.twist.twist.linear.x = sqrt(pow(base_pose.pose.position.x - old_x, 2) + pow(base_pose.pose.position.y - old_y, 2)) / (my_maximus_odom.header.stamp.toSec() - old_time.toSec());
+		my_maximus_odom.twist.twist.linear.y = 0;
+		my_maximus_odom.twist.twist.linear.z = 0;
+		my_maximus_odom.twist.twist.angular.x = 0;
+		my_maximus_odom.twist.twist.angular.y = 0;
+		my_maximus_odom.twist.twist.angular.z = (tf::getYaw (my_maximus_odom.pose.pose.orientation) - old_theta) / (my_maximus_odom.header.stamp.toSec() - old_time.toSec());
+
+		odom_pub.publish(my_maximus_odom);
+
+		old_x = base_pose.pose.position.x;
+		old_y = base_pose.pose.position.y;
+		old_theta = tf::getYaw (my_maximus_odom.pose.pose.orientation);
+		old_time = my_maximus_odom.header.stamp;
+
+                ROS_INFO("base_laser: (%.2f, %.2f. %.2f) -----> base_link: (%.2f, %.2f, %.2f) at time %.2f",
+                        my_maximus_odom.pose.pose.position.x, my_maximus_odom.pose.pose.position.y, my_maximus_odom.pose.pose.position.z,
+                        my_maximus_odom.twist.twist.linear.x, my_maximus_odom.twist.twist.linear.y, my_maximus_odom.twist.twist.angular.z, base_pose.header.stamp.toSec());
+
+        }
+        catch(tf::TransformException& ex){
+                ROS_ERROR("Received an exception trying to transform a point from \"odom\" to \"base_link\": %s", ex.what());
+        }
+
+
 }
 
 
@@ -290,9 +249,16 @@ int main(int argc, char **argv)
     TransformPose transform_Pose;
     // Refresh rate
     ros::Rate loop_rate(5);                                // 35 with bluetooth
+
+    tf::TransformListener listener(ros::Duration(10));
+
+
     while (ros::ok()) {
         ros::spinOnce();
         loop_rate.sleep();
+
+        transform_Pose.publish_all(boost::ref(listener));
+
     }
 
     ros::Duration(2.0).sleep();
--- maximus_setup_tf/src/tf_broadcaster.cpp
+++ maximus_setup_tf/src/tf_broadcaster.cpp
@@ -6,13 +6,17 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "maximus_tf_publisher");
     ros::NodeHandle n;
 
-    ros::Rate r(50);
+    ros::Rate r(100);
 
     tf::TransformBroadcaster broadcaster;
 
     while (n.ok()) {
         broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.1)),
                                                        ros::Time::now(), "base_link", "neato_laser"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.1)),
+                                                       ros::Time::now(), "base_link", "laser_frame"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)),
+                                                       ros::Time::now(), "map", "odom"));
         r.sleep();
     }
 }
