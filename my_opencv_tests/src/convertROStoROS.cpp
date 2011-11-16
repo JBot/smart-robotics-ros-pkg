8390228695cac3bee4efbe5159c9f4df9538eea9
Jbot <asso.smart@gmail.com>
2011-11-16 22:39:45 +0100

    Add feature detection for image coming from ROS.

--- my_opencv_tests/src/convertROStoROS.cpp
+++ my_opencv_tests/src/convertROStoROS.cpp
@@ -45,8 +45,43 @@ public:
       return;
     }
 
+      Mat gray;
+      cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
+      // smooth it, otherwise a lot of false circles may be detected
+      GaussianBlur( gray, gray, Size(9, 9), 2, 2 );
+      vector<Vec3f> circles;
+      HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 2, 200, 200, 100, 50 );
+      cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
+      for( size_t i = 0; i < circles.size(); i++ )
+      {
+         Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+         int radius = cvRound(circles[i][2]);
+         // draw the circle center
+         circle( cv_ptr->image, center, 3, Scalar(0,255,0), -1, 8, 0 );
+         // draw the circle outline
+         circle( cv_ptr->image, center, radius, Scalar(0,0,255), 3, 8, 0 );
+      }
+    Canny( gray, gray, 16, 33, 3 );
+    vector<Vec4i> lines;
+    HoughLinesP( gray, lines, 1, CV_PI/180, 10.0, 240.0, 15.0 );
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        if( ((lines.size() - i) > 1) && (sqrt((lines[i][0]-lines[i+1][0])*(lines[i][0]-lines[i+1][0]) + (lines[i][1]-lines[i+1][1])*(lines[i][1]-lines[i+1][1])) > 125 ) && (sqrt((lines[i][0]-lines[i+1][0])*(lines[i][0]-lines[i+1][0]) + (lines[i][1]-lines[i+1][1])*(lines[i][1]-lines[i+1][1])) < 170 )) {
+          line( cv_ptr->image, Point(lines[i][0], lines[i][1]),
+            Point(lines[i+1][2], lines[i+1][3]), Scalar(255,255,0), 3, 8 );
+          line( cv_ptr->image, Point(lines[i+1][0], lines[i+1][1]),
+            Point(lines[i][2], lines[i][3]), Scalar(255,255,0), 3, 8 );
+          i++;
+          break;
+        }
+        else {
+          line( cv_ptr->image, Point(lines[i][0], lines[i][1]),
+            Point(lines[i][2], lines[i][3]), Scalar(255,0,0), 3, 8 );
+        }
+    }
 
-    Mat gray = Mat::zeros(cv_ptr->image.rows, cv_ptr->image.cols, CV_8UC3);
+
+/*    Mat gray = Mat::zeros(cv_ptr->image.rows, cv_ptr->image.cols, CV_8UC3);
     Mat out;
     cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
 
@@ -57,6 +92,7 @@ public:
     Mat gray2;
     cvtColor(cv_ptr->image, gray2, CV_BGR2GRAY);
     //Canny( gray2, out, 16, 33, 3 );
+*/
 /*
     vector<Vec3f> circles;
     HoughCircles(out, circles, CV_HOUGH_GRADIENT, 3, 506, 90, 56, 50 );
@@ -88,7 +124,7 @@ public:
         line( cv_ptr->image, pt1, pt2, Scalar(0,0,255), 3, 8 );
     }
 */
-
+/*
     vector<Vec4i> lines;
     HoughLinesP( out, lines, 1, CV_PI/180, 10.0, 140.0, 15.0 );
     for( size_t i = 0; i < lines.size(); i++ )
@@ -96,11 +132,8 @@ public:
         line( cv_ptr->image, Point(lines[i][0], lines[i][1]),
             Point(lines[i][2], lines[i][3]), Scalar(255,0,0), 3, 8 );
     }
+*/
 
-
-
-    namedWindow( "Source", 1 );
-    imshow( "Source", out );
 
 /*    cv::namedWindow( "Source", 1 );
     cv::imshow( "Source", cv_ptr->image );
